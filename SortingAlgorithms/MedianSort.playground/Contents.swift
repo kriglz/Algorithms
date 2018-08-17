cc79e1285c475852ba2f9e5463b5462dcd9db2f9
Kristina Gelzinyte <kristina.gelzinyte@gmail.com>
2018-08-17 12:52:53 -0700

    rand selection of pivot index

--- SortingAlgorithms/MedianSort.playground/Contents.swift
+++ SortingAlgorithms/MedianSort.playground/Contents.swift
@@ -3,10 +3,9 @@
 import UIKit
 
 var sortingArray = [15, 09, 08, 01, 04, 11, 07, 12, 13, 06, 05, 03, 16, 02, 10, 14]
-let pivotIndex = 6
 
-let rightIndex = sortingArray.count - 1
-let leftIndex = 0
+//let rightIndex = sortingArray.count - 1
+//let leftIndex = 0
 
 func compare(numberA: Int, numberB: Int) -> Int {
     if numberA > numberB {
@@ -15,23 +14,57 @@ func compare(numberA: Int, numberB: Int) -> Int {
     return 0
 }
 
-func partition() {
+func partition(leftIndex: Int, rightIndex: Int, pivotIndex: Int) {
     let pivot = sortingArray[pivotIndex]
     sortingArray.swapAt(pivotIndex, rightIndex)
 
     var storeIndex = leftIndex
-    
+
     for index in leftIndex..<rightIndex {
         if compare(numberA: sortingArray[index], numberB: pivot) <= 0 {
             sortingArray.swapAt(index, storeIndex)
             storeIndex += 1
-            
+
             print(sortingArray)
         }
     }
-    
+
     sortingArray.swapAt(rightIndex, storeIndex)
 }
 
-partition()
+extension Range {
+    /// Returns random `Int` number of specific range.
+    var random: Int {
+        get {
+            let mini = UInt32(lowerBound as! Int)
+            let maxi = UInt32(upperBound as! Int)
+            
+            return Int(mini + arc4random_uniform(maxi - mini))
+        }
+    }
+}
+
+func selectPivotIndex(leftIndex: Int, rightIndex: Int) -> Int {
+    let range: Range = leftIndex..<rightIndex
+    return range.random
+}
+
+func selectKthIndex(kIndex: Int, leftIndex: Int, rightIndex: Int) -> Int {
+    let pivotIndex = selectPivotIndex(leftIndex: leftIndex, rightIndex: rightIndex)
+    
+    partition(leftIndex: leftIndex, rightIndex: rightIndex, pivotIndex: pivotIndex)
+    
+    if leftIndex + kIndex + 1 < pivotIndex {
+        return pivotIndex
+    }
+    
+    if leftIndex + kIndex - 1 < pivotIndex {
+        return selectKthIndex(kIndex: kIndex, leftIndex: leftIndex, rightIndex: pivotIndex - 1)
+    } else {
+        return selectKthIndex(kIndex: kIndex - (pivotIndex - leftIndex + 1), leftIndex: pivotIndex + 1, rightIndex: rightIndex)
+    }
+}
+
+
+
 
