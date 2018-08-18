ebe874e7bfd1bd5637ffe1e0890f08b5a48e7554
Kristina Gelzinyte <kristina.gelzinyte@gmail.com>
2018-08-17 17:22:25 -0700

    fixed the bug

--- SortingAlgorithms/MedianSort.playground/Contents.swift
+++ SortingAlgorithms/MedianSort.playground/Contents.swift
@@ -4,6 +4,7 @@ import UIKit
 
 var sortingArray = [15, 09, 08, 01, 04, 11, 07, 12, 13, 06, 05, 03, 16, 02, 10, 14]
 
+/// Compares two numbers.
 func compare(numberA: Int, numberB: Int) -> Int {
     if numberA > numberB {
         return 1
@@ -11,6 +12,7 @@ func compare(numberA: Int, numberB: Int) -> Int {
     return 0
 }
 
+/// Sorts by partitioning around pivot number.
 func partition(leftIndex: Int, rightIndex: Int, pivotIndex: Int) -> Int {
     let pivot = sortingArray[pivotIndex]
     sortingArray.swapAt(pivotIndex, rightIndex)
@@ -29,6 +31,7 @@ func partition(leftIndex: Int, rightIndex: Int, pivotIndex: Int) -> Int {
 }
 
 extension Range {
+
     /// Returns random `Int` number of specific range.
     var random: Int {
         get {
@@ -40,27 +43,38 @@ extension Range {
     }
 }
 
-/// Return random pivot index of specific range
+/// Return random pivot index of specific range.
 func selectPivotIndex(leftIndex: Int, rightIndex: Int) -> Int {
     let range: Range = leftIndex..<rightIndex
     return range.random
 }
 
+/// 
 func selectKthIndex(kIndex: Int, leftIndex: Int, rightIndex: Int) {
     guard rightIndex >= leftIndex else { return }
 
     let randomPivotIndex = selectPivotIndex(leftIndex: leftIndex, rightIndex: rightIndex)
+    print(sortingArray)
+
     let pivotIndex = partition(leftIndex: leftIndex, rightIndex: rightIndex, pivotIndex: randomPivotIndex)
     
-    if leftIndex + kIndex + 1 < pivotIndex { return }
+    print(sortingArray)
+    print(leftIndex, rightIndex, "range")
+    print(randomPivotIndex, "randomPivotIndex")
+    print(pivotIndex, "pivotIndex")
+    print(kIndex, "kIndex\n")
+    
+    if leftIndex + kIndex - 1 == pivotIndex { return }
     
     if leftIndex + kIndex - 1 < pivotIndex {
+        print("left")
         selectKthIndex(kIndex: kIndex, leftIndex: leftIndex, rightIndex: pivotIndex - 1)
     } else {
         selectKthIndex(kIndex: kIndex - (pivotIndex - leftIndex + 1), leftIndex: pivotIndex + 1, rightIndex: rightIndex)
     }
 }
 
+/// Performs median sort.
 func medianSort(leftIndex: Int, rightIndex: Int) {
     guard rightIndex > leftIndex else { return }
 
