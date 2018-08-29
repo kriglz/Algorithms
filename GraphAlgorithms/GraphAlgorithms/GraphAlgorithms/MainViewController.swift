b0bed65f8be81deb0408808c411da6d860b8a605
Kristina Gelzinyte <kristina.gelzinyte@gmail.com>
2018-08-29 15:33:29 -0700

    Refactoring

---
+++ GraphAlgorithms/GraphAlgorithms/GraphAlgorithms/MainViewController.swift
@@ -0,0 +1,30 @@
+//
+//  MainViewController.swift
+//  GraphAlgorithms
+//
+//  Created by Kristina Gelzinyte on 8/29/18.
+//  Copyright © 2018 Kristina Gelzinyte. All rights reserved.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+    
+    // MARK: - Properties
+    
+    private let maze = Maze(columns: 4, rows: 4)
+
+    override var prefersStatusBarHidden: Bool {
+        return true
+    }
+    
+    // MARK: - Life cycle fucntions
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .black
+        
+        view.addSubview(maze.view())
+    }
+}
