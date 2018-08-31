4083a0bd49a3d4c7a4371e2c937f2db89a9eb627
Kristina Gelzinyte <kristina.gelzinyte@gmail.com>
2018-08-31 11:01:35 -0700

    Queue

---
+++ GraphAlgorithms/GraphAlgorithms/GraphAlgorithms/Algorithms/Queue.swift
@@ -0,0 +1,53 @@
+//
+//  Queue.swift
+//  GraphAlgorithms
+//
+//  Created by Kristina Gelzinyte on 8/31/18.
+//  Copyright © 2018 Kristina Gelzinyte. All rights reserved.
+//
+
+import UIKit
+
+/// A queue - array type object to store queues.
+public struct Queue<T> {
+    
+    // MARK: - Private properties
+    
+    /// An array to store queues.
+    private var array: [T]
+    
+    // MARK: - Public properties
+    
+    /// Returns true if queus list is empty.
+    var isEmpty: Bool {
+        return array.isEmpty
+    }
+    
+    /// Returns first queue in the list.
+    var first: T? {
+        return array.first
+    }
+    
+    // MARK: - Initialization
+    
+    /// Initializes empty queue list.
+    init() {
+        array = []
+    }
+    
+    // MARK: - Queue list update
+    
+    /// Adds an element to the end of the queue.
+    mutating func push(_ element: T) {
+        array.append(element)
+    }
+    
+    /// Removes first queue object in the list.
+    @discardableResult mutating func pop() -> T? {
+        if isEmpty {
+            return nil
+        } else {
+            return array.removeFirst()
+        }
+    }
+}
