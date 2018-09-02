35eba3a2409a6340a10df8ed3c923cd604dbd62c
Kristina Gelzinyte <kristina.gelzinyte@gmail.com>
2018-09-01 17:09:59 -0700

    Implemented DijkstrasPriorityQueueAlgorithm

--- GraphAlgorithms/GraphAlgorithms/GraphAlgorithms/Algorithms/DijkstrasPriorityQueueAlgorithm .swift
+++ GraphAlgorithms/GraphAlgorithms/GraphAlgorithms/Algorithms/DijkstrasPriorityQueueAlgorithm .swift
@@ -19,32 +19,42 @@ class DijkstrasPriorityQueueAlgorithm {
     
     func search(in vertexList: [Vertex], size: VertexListSize) -> [Vertex] {
         self.size = size
-        let startIndex = 0 // (size.columns * size.rows) / 2
+        let startIndex = (size.columns * size.rows) / 2
         return searchSingleSourceShortest(for: startIndex, in: vertexList)
     }
     
     private func searchSingleSourceShortest(for index: Int, in vertexList: [Vertex]) -> [Vertex] {
         let maxVertexIndex = vertexList.count - 1
         var queue = Queue<Vertex>()
-
-        vertexList[index].distance = 0
         
         // Adds all vertex nodes to the queue.
         for index in 0...maxVertexIndex {
             queue.push(vertexList[index])
         }
         
+        // Set source vertex distance to 0.
+        vertexList[index].distance = 0
+        
         // Finds vertex in ever-shrinking set, whose distance is smallest.
         // Recomputes potential new paths to update allshortest paths.
         while !queue.isEmpty {
-            let smallest = queue.smallest
-            print(smallest)
+            let smallestDistanceVertex = queue.smallest!
+            smallestDistanceVertex.stateColor = .black
+            queue.pop(vertex: smallestDistanceVertex)
             
-            
+            let neighbourList = smallestDistanceVertex.availableNeighbourVertexList(in: vertexList, with: size)
+            neighbourList.forEach {
+                // Weight/distance to neighbour vertex nodes is always set to 1.
+                let alternativeDistance = smallestDistanceVertex.distance + 1
+                
+                if alternativeDistance < $0.distance {
+                    $0.stateColor = .gray
+                    $0.distance = alternativeDistance
+                    $0.predecessorIndex = smallestDistanceVertex.index
+                }
+            }
         }
-        
-        
-        
+
         return vertexList
     }
 }
