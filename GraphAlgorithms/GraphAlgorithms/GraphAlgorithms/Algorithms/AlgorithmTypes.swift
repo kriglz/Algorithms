3fb659e4351b85f876e19d35fbd819c1749ad0d1
Kristina Gelzinyte <kristina.gelzinyte@gmail.com>
2018-09-04 10:55:50 -0700

    Algorithm types

---
+++ GraphAlgorithms/GraphAlgorithms/GraphAlgorithms/Algorithms/AlgorithmTypes.swift
@@ -0,0 +1,22 @@
+//
+//  AlgorithmTypes.swift
+//  GraphAlgorithms
+//
+//  Created by Kristina Gelzinyte on 9/4/18.
+//  Copyright © 2018 Kristina Gelzinyte. All rights reserved.
+//
+
+/// Grapth search algorithm types.
+///
+/// List of possible algorithms:
+/// - Depth-First search
+/// - Breadth-First search
+/// - Dijkstra's Single Source Shortest Path search
+/// - Prim's Minimum Spanning Tree search
+enum AlgorithmTypes {
+    
+    case depthFirst
+    case breadthFirst
+    case dijkstras
+    case prims
+}
