4ca8e53dacdf3935032027fdfffe26272d009627
Kristina Gelzinyte <kristina.gelzinyte@gmail.com>
2018-09-19 14:50:35 -0700

    Cleanup

--- PathFindingInAI/A*Search.playground/Pages/Untitled Page.xcplaygroundpage/Contents.swift
+++ PathFindingInAI/A*Search.playground/Pages/Untitled Page.xcplaygroundpage/Contents.swift
@@ -1,66 +1,70 @@
 import UIKit
 
-var str = "Hello, playground"
-
 class Move {
     
     var hasNext = false
     var next: Move?
-    
-    func execute(_ successorNode: Node) {
-        
-    }
-    
 }
 
-class Node {
+class DepthTransition {
+    
+    var move: Move
+    var node: Node
+    var depth: Int
+    
+    init(move: Move, node: Node, depth: Int) {
+        self.move = move
+        self.node = node
+        self.depth = depth
+    }
+}
+
+class Node: NSObject {
     
     /// Associates an integer score with the board state, representing the result of the evaluation funtion.
     private(set) var score = 0
-    
     /// Object used to compare two nodes.
     private(set) var key = 0
+    /// Returns stored transition depth data.
+    private(set) var storedData: DepthTransition?
     
+    /// Comparison operator
+    static func == (lhs: Node, rhs: Node) -> Bool {
+        return lhs.key == rhs.key
+    }
+    
+    /// Evaluates the score for current board state.
     func evaluateScore() {
+        // TODO: - evaluate score
         let score = 0
         self.score = score
     }
     
     /// Returns a list of available moves for a board state.
     func validMoves() -> [Move] {
+        // TODO: - evaluate possible moves
         return []
     }
     
     /// Returns the identical copy of the board state.
-    func copy() -> Node {
-        return self
+    func clone() -> Node {
+        let copyNode = self.copy() as! Node
+        copyNode.key = self.key
+        return copyNode
     }
-    
-    /// Determines weather two board states are equal.
-//    func equivalent(to node: Node) -> Bool {
-//        // if key == key
-//        return false
-//    }
  
     /// Associates the given object with the board state to be used by search algorithm.
-    /// Returns transition depth data.
-    func storedData() -> Int? {
-        // Stored data object should have transition depth (Int) info.
-        return nil
+    func storeData(data: DepthTransition) {
+        self.storedData = data
     }
-}
-
-extension Node: Equatable {
     
-    static func == (lhs: Node, rhs: Node) -> Bool {
-        return lhs.key == rhs.key
+    func execute(move: Move) {
+        // TODO: - Move the node
     }
 }
 
 /// A queue - array type object to store queues.
 public struct Queue<T> {
-    
-    // MARK: - Private properties
     
     /// An array to store queues.
     private var array: [T]
@@ -92,43 +96,24 @@ public struct Queue<T> {
         return first as? Node
     }
     
-    // MARK: - Initialization
-    
     /// Initializes empty queue list.
     init() {
         array = []
     }
     
-    // MARK: - Queue list update
-    
     /// Adds an element to the end of the queue.
-    ///
-    /// - Parameters:
-    ///     - element: An element to be added to the queue.
     mutating func push(_ element: T) {
         array.append(element)
     }
     
-    /// Removes first queue object in the queue list.
-    @discardableResult mutating func pop() -> T? {
-        if isEmpty {
-            return nil
-        } else {
-            return array.removeFirst()
-        }
-    }
-    
     /// Removes specified queue object in the list.
-    ///
-    /// - Parameters:
-    ///     - node: An object of a type `Node` to be removed from the queue list.
     mutating func pop(node: Node) {
         if !isEmpty, let nodeArray = array as? [Node], let index = nodeArray.index(where: { $0 == node }) {
             array.remove(at: index)
         }
     }
     
-    // MARK: - Queue list check
+    /// Checks and returns the node if its in the queue.
     func contains(node: Node) -> Node? {
         if let nodeArray = array as? [Node], let equivalentNode = nodeArray.first(where: { $0.key == node.key }) {
             return equivalentNode
@@ -138,21 +123,16 @@ public struct Queue<T> {
     }
 }
 
-
 struct ASearch {
-    
-    func score(node: Node) -> Int {
-        return 0
-    }
-    
+
     func search(initialNode: Node, goalNode: Node) -> (initial: Node, solution: Node?) {
         var openNodeSet = Queue<Node>()
-        
-        let initialNodeCopy = initialNode.copy()
-        initialNodeCopy.evaluateScore()
-        openNodeSet.push(initialNodeCopy)
-        
         var closedNodeSet = Queue<Node>()
+
+        let initialNodeCopy = initialNode.clone()
+        initialNodeCopy.evaluateScore()
+        
+        openNodeSet.push(initialNodeCopy)
         
         while !openNodeSet.isEmpty {
             // Removes node with smallest evaluation function and marks it as closed.
@@ -172,8 +152,8 @@ struct ASearch {
             
             // Compute successor moves and update open/closed lists.
             var depth = 1
-            if let transitionDepth = smallestScoreOpenNode.storedData() { // retrieves stored data
-                depth = transitionDepth + 1
+            if let transition = smallestScoreOpenNode.storedData { // retrieves stored data
+                depth = transition.depth + 1
             }
             
             let moves = smallestScoreOpenNode.validMoves()
@@ -185,11 +165,12 @@ struct ASearch {
                 }
                     
                 // Make the move and score the board state.
-                let successorNode = smallestScoreOpenNode.copy()
-                nextMove.execute(successorNode)
+                let successorNode = smallestScoreOpenNode.clone()
+                successorNode.execute(move: nextMove)
                 
                 // Record previous move for solution trace and compute evaluation function to see if we have improved upon a state already closed.
-                successorNode.storedData() // should add new transition deoth to stored data
+                let transitionDepth = DepthTransition(move: nextMove, node: smallestScoreOpenNode, depth: depth)
+                successorNode.storeData(data: transitionDepth)
                 successorNode.evaluateScore()
                 
                 // If already visited, see if we are revisiting with lower cost.
