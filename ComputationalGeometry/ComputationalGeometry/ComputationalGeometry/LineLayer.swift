ae1dfdab0368d8a716e319c19d54f5b538f808a6
Kristina Gelzinyte <kristina.gelzinyte@gmail.com>
2018-09-27 16:59:33 -0700

    Line layer init

---
+++ ComputationalGeometry/ComputationalGeometry/ComputationalGeometry/LineLayer.swift
@@ -0,0 +1,25 @@
+//
+//  LineLayer.swift
+//  ComputationalGeometry
+//
+//  Created by Kristina Gelzinyte on 9/27/18.
+//  Copyright © 2018 Kristina Gelzinyte. All rights reserved.
+//
+
+import UIKit
+
+class LineLayer: CAShapeLayer {
+    
+    private(set) var uuid: String!
+    
+    convenience init(from path: CGPath, color: CGColor = UIColor.white.cgColor, with uuid: String) {
+        self.init()
+        
+        self.uuid = uuid
+        
+        self.path = path
+        self.strokeColor = color
+        self.lineWidth = 3
+        self.opacity = 0
+    }
+}
