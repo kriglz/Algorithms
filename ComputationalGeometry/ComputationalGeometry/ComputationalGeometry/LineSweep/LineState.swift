46d78e24ee34c4f70aa7b330b5ed8c7e5b10c30a
Kristina Gelzinyte <kristina.gelzinyte@gmail.com>
2018-10-03 14:29:59 -0700

    Line state methods

--- ComputationalGeometry/ComputationalGeometry/ComputationalGeometry/LineSweep/LineState.swift
+++ ComputationalGeometry/ComputationalGeometry/ComputationalGeometry/LineSweep/LineState.swift
@@ -13,7 +13,7 @@ class LineState {
     // MARK: - Properties
 
     private(set) var sweepPoint: CGPoint!
-    private var states = [LineSegment]()
+    private var state = AugmentedBalancedTree()
     
     // MARK: - Sweep point update
     
@@ -24,38 +24,78 @@ class LineState {
     // MARK: - Line segment update
     
     func insertSegment(_ segments: [LineSegment]) {
-        states.append(contentsOf: segments)
+
     }
     
-    func successor(for lineSegment: LineSegment) -> LineSegment? {
-        if let lineSegmentIndex = states.firstIndex(where: { $0 == lineSegment }), (0..<states.count).contains(lineSegmentIndex + 1) {
-            return states[lineSegmentIndex + 1]
+    func successor(for node: AugmentedBalancedBinaryNode) -> AugmentedBalancedBinaryNode? {
+        var successor = node
+        
+        // If we are the right-child of a node, must go back.
+        while successor != state.root, successor == successor.parent.right {
+            successor = successor.parent
         }
+        
+        // Now we have reached a node by whose parent we are not the right. If we are indeed at the root, then no successor.
+        if successor == state.root {
+            return nil
+        }
+        
+        // Otherwise go to the right, and find the left-most child. This node is the successor.
+        guard let right = successor.parent.right else {
+            return nil
+        }
+        
+        successor = right
+        
+        while successor.left != nil {
+            successor = successor.left!
+        }
+        
+        return successor
+    }
+    
+    func predecessor(for node: AugmentedBalancedBinaryNode) -> AugmentedBalancedBinaryNode? {
+        var predecessor = node
+        
+        // If we are the left-child of a node, must go back.
+        while predecessor != state.root, predecessor == predecessor.parent.left {
+            predecessor = predecessor.parent
+        }
+        
+        // Now we have reached a node by whose parent we are not the right. If we are indeed at the root, then no successor.
+        if predecessor == state.root {
+            return nil
+        }
+        
+        // Otherwise go to the left, and find the right-most child. This node is the pred
+        guard let left = predecessor.parent.left else {
+            return nil
+        }
+        
+        predecessor = left
+        
+        while predecessor.right != nil {
+            predecessor = predecessor.right!
+        }
+        
+        return predecessor
+    }
+    
+    func leftNeighbour(for eventPoint: EventPoint) -> AugmentedBalancedBinaryNode? {
         return nil
     }
     
-    func predecessor(for lineSegment: LineSegment) -> LineSegment? {
-        if let lineSegmentIndex = states.firstIndex(where: { $0 == lineSegment }), (0..<states.count).contains(lineSegmentIndex - 1) {
-            return states[lineSegmentIndex - 1]
-        }
+    func rightNeighbour(for eventPoint: EventPoint) -> AugmentedBalancedBinaryNode? {
         return nil
-    }
-    
-    func leftNeighbourSegment(for eventPoint: EventPoint) -> LineSegment? {
-        return LineSegment(startPoint: .zero, endPoint: .zero)
-    }
-    
-    func rightNeighbourSegment(for eventPoint: EventPoint) -> LineSegment? {
-        return LineSegment(startPoint: .zero, endPoint: .zero)
     }
     
     // MARK: - Intersection point evaluation
 
-    func determineIntersecting(eventPoint: EventPoint, leftSegment: LineSegment?, rightSegment: LineSegment?) {
+    func determineIntersecting(eventPoint: EventPoint, left: AugmentedBalancedBinaryNode?, right: AugmentedBalancedBinaryNode?) {
         
     }
     
-    func deleteSegmentRange(left: LineSegment?, right: LineSegment?) {
+    func deleteSegmentRange(left: AugmentedBalancedBinaryNode?, right: AugmentedBalancedBinaryNode?) {
         
     }
 }
