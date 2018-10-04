ebba827d12f1559ccd82467af7df7209f543e1ea
Kristina Gelzinyte <kristina.gelzinyte@gmail.com>
2018-10-04 10:03:52 -0700

    Point location evaluation

--- ComputationalGeometry/ComputationalGeometry/ComputationalGeometry/LineSweep/LineSegment.swift
+++ ComputationalGeometry/ComputationalGeometry/ComputationalGeometry/LineSweep/LineSegment.swift
@@ -10,9 +10,6 @@ import UIKit
 
 class LineSegment {
  
-    private(set) var start: CGPoint
-    private(set) var end: CGPoint
-    
     var cgPath: CGPath {
         let path = UIBezierPath()
         path.move(to: start)
@@ -20,23 +17,102 @@ class LineSegment {
         return path.cgPath
     }
     
+    private(set) var start: CGPoint
+    private(set) var end: CGPoint
+    
+    private var sign: Int = 0
+    private var slope: CGFloat = 0
+    
+    private var isHorizontal: Bool {
+        return start.y == end.y
+    }
+    
+    private var isVertical: Bool {
+        return start.x == end.x
+    }
+
     init(startPoint: CGPoint, endPoint: CGPoint) {
         self.start = startPoint
         self.end = endPoint
+        
+        // Evaluate sign and slope.
+        evaluateSlopeAndSign()
+    }
+    
+    /// Evaluates sign and slope of the line segment.
+    private func evaluateSlopeAndSign() {
+        if start.x == end.x {
+            slope = CGFloat.nan
+            sign = 1
+        } else {
+            slope = (end.y - start.y) / (end.x - start.x)
+            
+            if slope < 0 {
+                sign = -1 
+            } else {
+                // Even for 0/horizontal case.
+                sign = 1
+            }
+        }
+        
     }
     
     func intersectionPoint(with lineSegment: LineSegment) -> CGPoint? {
         return .zero
     }
     
+    /**
+     * Given the line segment in its reverse-directed orientation (from lower to upper
+     * point), determine if point p is to the right of the line, as you head from lower
+     * to upper.
+     *
+     * If it is exactly on the line, then we return false.
+     *
+     * Note that we use the following inverted equation for lines to make the determination.
+     * The equation can easily be confirmed, but care must be taken based upon the sign
+     * of the slope (since we are dividing by m).
+     *
+     *                 (y2 - y1)
+     *    0 = y - y1 - --------- (x - x1)
+     *                 (x2 - x1)
+     *
+     * Since this operation is SO COMMON, we use a standard trick to remove the IF statements.
+     * Specifically, if m is positive, then we check if res &lt; 0 but if m is negative, then we
+     * check if -res &lt; 0. Thus we also need to store the SIGN of the slope with each segment.
+     */
+    
     /// Determine if the given point is to the right of the line segment, if we view the line segment from the lower (end) point to the upper (start) point.
     func pointOnRight(of point: CGPoint) -> Bool {
-        return false
+        if isVertical {
+            return point.x > self.start.x
+        } else if isHorizontal {
+            return point.y > self.start.y
+        }
+        
+        let res = point.y - end.y - slope * (point.x - end.x)
+        return res * CGFloat(sign) < 0
     }
+    
+    /**
+     * Given the line segment in its reverse-directed orientation (from lower to upper point), determine
+     * if point p is to the left of the line, as you head from lower to upper
+     *
+     * If it is exactly on the line, then we return false.
+     *
+     * See {@link #pointOnRight(IPoint)} for the mathematical explanation
+     * for these equations, as well as importance of sign.
+     */
     
     /// Determine if the given point is to the left of the line segment, if we view the line segment from the lower (end) point to the upper (start) point.
     func pointOnLeft(of point: CGPoint) -> Bool {
-        return false
+        if isVertical {
+            return start.x > point.x
+        } else if isHorizontal {
+            return start.y > point.y
+        }
+        
+        let res = point.y - end.y - slope * (point.x - end.x)
+        return res * CGFloat(sign) > 0
     }
 }
 
--- ComputationalGeometry/ComputationalGeometry/ComputationalGeometry/LineSweep/LineSweepController.swift
+++ ComputationalGeometry/ComputationalGeometry/ComputationalGeometry/LineSweep/LineSweepController.swift
@@ -16,12 +16,21 @@ class LineSweepController {
     // MARK: - Initialization
     
     init(lineCount: Int, in rect: CGRect) {
-        for _ in 0...lineCount {
-            let start = CGPoint(x: CGFloat.random(in: rect.minX...rect.maxX), y: CGFloat.random(in: rect.minY...rect.maxY))
-            let end = CGPoint(x: CGFloat.random(in: rect.minX...rect.maxX), y: CGFloat.random(in: rect.minY...rect.maxY))
-            let newLine = LineSegment(startPoint: start, endPoint: end)
-            lineSegments.append(newLine)
-        }
+        let end = CGPoint(x: CGFloat.random(in: rect.minX...rect.maxX), y: CGFloat.random(in: rect.minY...rect.maxY))
+        let first = LineSegment(startPoint: .zero, endPoint: end)
+        
+        let endTwo = CGPoint(x: CGFloat.random(in: rect.minX...rect.maxX), y: CGFloat.random(in: rect.minY...rect.maxY))
+        let second = LineSegment(startPoint: .zero, endPoint: endTwo)
+        
+        lineSegments.append(first)
+        lineSegments.append(second)
+        
+//        for _ in 0...lineCount {
+//            let start = CGPoint(x: CGFloat.random(in: rect.minX...rect.maxX), y: CGFloat.random(in: rect.minY...rect.maxY))
+//            let end = CGPoint(x: CGFloat.random(in: rect.minX...rect.maxX), y: CGFloat.random(in: rect.minY...rect.maxY))
+//            let newLine = LineSegment(startPoint: start, endPoint: end)
+//            lineSegments.append(newLine)
+//        }
     }
     
     // MARK: - Compute methods
