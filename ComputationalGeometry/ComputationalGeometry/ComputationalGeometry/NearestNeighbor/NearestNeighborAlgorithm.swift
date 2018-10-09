2cde43ca274ac2ff13528ccaf94ddc5a30a7293e
Kristina Gelzinyte <kristina.gelzinyte@gmail.com>
2018-10-09 16:40:11 -0700

    Main class init

---
+++ ComputationalGeometry/ComputationalGeometry/ComputationalGeometry/NearestNeighbor/NearestNeighborAlgorithm.swift
@@ -0,0 +1,13 @@
+//
+//  NearestNeighborAlgorithm.swift
+//  ComputationalGeometry
+//
+//  Created by Kristina Gelzinyte on 10/9/18.
+//  Copyright © 2018 Kristina Gelzinyte. All rights reserved.
+//
+
+import UIKit
+
+class NearestNeighborAlgorithm {
+    
+}
