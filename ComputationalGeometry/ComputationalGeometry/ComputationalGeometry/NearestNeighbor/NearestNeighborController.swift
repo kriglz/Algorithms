62865b18acc31b0a07dd9689cbabc0decb62de5c
Kristina Gelzinyte <kristina.gelzinyte@gmail.com>
2018-10-17 17:30:10 +0300

    Action execution

--- ComputationalGeometry/ComputationalGeometry/ComputationalGeometry/NearestNeighbor/NearestNeighborController.swift
+++ ComputationalGeometry/ComputationalGeometry/ComputationalGeometry/NearestNeighbor/NearestNeighborController.swift
@@ -14,14 +14,55 @@ class NearestNeighborController {
     
     private(set) var points: [CGPoint]
     private let algorithm: NearestNeighborAlgorithm
+    private let frame: CGRect
+    
+    var treeActionBuffer: [LineDrawingAction] {
+        guard !algorithm.treeActionBuffer.isEmpty else {
+            return []
+        }
+        
+        var actions = [LineDrawingAction]()
+        
+        for action in algorithm.treeActionBuffer {
+            var startPoint = action.node.point
+            var endPoint = action.node.parentPoint
 
-    var treeActionBuffer: [TreeLineDrawingAction] {
-        return algorithm.treeActionBuffer
+            if endPoint == nil {
+                endPoint = startPoint
+                switch action.node.dimension {
+                case 1:
+                    startPoint.y = frame.minY
+                    endPoint?.y = frame.maxY
+                default:
+                    startPoint.x = frame.minX
+                    endPoint?.x = frame.maxX
+                }
+            } else {
+                switch action.node.dimension {
+                case 1:
+//                    if endPoint!.x < action.node.point.x {
+//                        startPoint.y = frame.minY
+//                    } else {
+//                        startPoint.y = frame.maxY
+//                    }
+                    endPoint?.x = action.node.point.x
+                default:
+                    endPoint?.y = action.node.point.y
+                }
+            }
+            
+            let line = Line(startPoint: startPoint, endPoint: endPoint!)
+            let lineAction = LineDrawingAction(line: line, type: .addition, index: action.index)
+            actions.append(lineAction)
+        }
+        
+        return actions
     }
     
     // MARK: - Initialization
     
     init(pointCount: Int, in rect: CGRect) {
+        self.frame = rect
         points = []
         
         for _ in 1...pointCount {
