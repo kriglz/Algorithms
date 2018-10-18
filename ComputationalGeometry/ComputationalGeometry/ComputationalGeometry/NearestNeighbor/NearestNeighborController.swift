704ac6c0540c6942037e6ce08ea38fac29ba9329
Kristina Gelzinyte <kristina.gelzinyte@gmail.com>
2018-10-18 22:19:24 +0300

    improved animation

--- ComputationalGeometry/ComputationalGeometry/ComputationalGeometry/NearestNeighbor/NearestNeighborController.swift
+++ ComputationalGeometry/ComputationalGeometry/ComputationalGeometry/NearestNeighbor/NearestNeighborController.swift
@@ -25,33 +25,88 @@ class NearestNeighborController {
         
         for action in algorithm.treeActionBuffer {
             var startPoint = action.node.point
-            var endPoint = action.node.parentPoint
-
-            if endPoint == nil {
-                endPoint = startPoint
+            var endPoint = action.node.point
+            
+            var parent = action.node.parent
+            
+            if parent == nil {
                 switch action.node.dimension {
                 case 1:
                     startPoint.y = frame.minY
-                    endPoint?.y = frame.maxY
+                    endPoint.y = frame.maxY
                 default:
                     startPoint.x = frame.minX
-                    endPoint?.x = frame.maxX
+                    endPoint.x = frame.maxX
                 }
             } else {
+                endPoint = parent!.point
+                
                 switch action.node.dimension {
                 case 1:
-//                    if endPoint!.x < action.node.point.x {
-//                        startPoint.y = frame.minY
-//                    } else {
-//                        startPoint.y = frame.maxY
+                    endPoint.x = action.node.point.x
+
+//                    if parent?.parent == nil {
+//                        if endPoint.y < action.node.point.y {
+//                            startPoint.y = frame.maxY
+//                        } else {
+//                            startPoint.y = frame.minY
+//                        }
 //                    }
-                    endPoint?.x = action.node.point.x
+                    
+                    while parent != nil {
+
+                        if parent?.parent == nil {
+                            if endPoint.y < action.node.point.y {
+                                startPoint.y = frame.maxY
+                            } else {
+                                startPoint.y = frame.minY
+                            }
+                        }
+                        
+                        if let parentParent = parent?.parent {
+                            if startPoint.y < parent!.point.y, startPoint.y > parentParent.point.y {
+                                startPoint.y = parentParent.point.y
+                            } else if startPoint.y > parent!.point.y, startPoint.y < parentParent.point.y {
+                                startPoint.y = parentParent.point.y
+                            }
+                        }
+                    
+                        parent = parent?.parent
+                    }
+                    
                 default:
-                    endPoint?.y = action.node.point.y
+                    endPoint.y = action.node.point.y
+                    
+                    if parent?.parent == nil, endPoint.x < action.node.point.x {
+                        startPoint.x = frame.maxX
+                    } else if parent?.parent == nil, endPoint.x > action.node.point.x {
+                        startPoint.x = frame.minX
+                    }
+                    
+                    while parent != nil {
+//                        if parent?.parent == nil {
+//                            if endPoint.x < action.node.point.x, startPoint.x < parent!.point.x {
+//                                startPoint.x = parent!.point.x //frame.maxX
+//                            } else if endPoint.x > action.node.point.x, startPoint.x > parent!.point.x {
+//                                startPoint.x = parent!.point.x //frame.minX
+//                            }
+//                        }
+                        
+                        
+                        if let parentParent = parent?.parent {
+                            if startPoint.x < parent!.point.x, startPoint.x > parentParent.point.x {
+                                startPoint.x = parentParent.point.x
+                            } else if startPoint.x > parent!.point.x, startPoint.x < parentParent.point.x {
+                                startPoint.x = parentParent.point.x
+                            }
+                        }
+                        
+                        parent = parent?.parent
+                    }
                 }
             }
             
-            let line = Line(startPoint: startPoint, endPoint: endPoint!)
+            let line = Line(startPoint: startPoint, endPoint: endPoint)
             let lineAction = LineDrawingAction(line: line, type: .addition, index: action.index)
             actions.append(lineAction)
         }
